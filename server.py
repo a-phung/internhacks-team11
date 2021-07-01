7a65e2d41d92128f2ab3569dc3585bc3d8dfe2e7
a-phung <andyphunghb@gmail.com>
2021-07-01 01:31:11 -0700

    Created routes for homepage and about page. Created skeleton/layout for html pages. Added home.html, about.html. Formatted homepage with Bootstrap and a main.css file.

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)  # Instantiates flask application
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
