13e8f197913b56d46c174ee80378a969dbc61873
a-phung <andyphunghb@gmail.com>
2021-07-02 04:54:42 -0700

    Added login.html and register.html. Added registration and login verification. Added new routes.

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)  # Instantiates flask application
+
+app.config["SECRET_KEY"] = "af1fad958cb13759f8627cc704452326"  # Will protect against modifying cookies
 
 
 @app.route("/")
@@ -11,6 +14,27 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html", title="About")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for("home"))
+    return render_template("register.html", title="Registration", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # TODO: Connect to database to verify login
+        flash("You are now logged in!", "success")
+        return redirect(url_for("home"))
+        # Else:
+        #     flash("Login Unsuccessful. Please check username and password", "danger")
+    return render_template("login.html", title="Login", form=form)
 
 
 if __name__ == "__main__":
